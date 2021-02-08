18404969c46f0f3ed8d9ac504165bb4f4534f338
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-02-08 16:16:18 +0600

    156 Reacting to parameter changes

--- src/ViewProduct.vue
+++ src/ViewProduct.vue
@@ -5,6 +5,17 @@
     <h1><strong>Price:</strong> {{ product.price | currency }}</h1>
     <h1><strong>In Stock:</strong> {{ product.inStock }}</h1>
     <h1>{{ product.description }}</h1>
+
+    <div v-if="relatedProducts != null">
+      <h2>Related Products</h2>
+      <ul>
+        <li v-for="related in relatedProducts">
+          <router-link :to="{ name: 'viewProduct', params: { productId: related.id } }">
+            {{ related.name }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -26,6 +37,11 @@ export default {
   created() {
     this.product = this.getProduct(this.productId);
   },
+  watch: {
+    productId(newValue, oldValue) {
+      this.product = this.getProduct(newValue);
+    },
+  },
   methods: {
     getProduct(productId) {
       let match = null;
@@ -36,6 +52,22 @@ export default {
         }
       });
       return match;
+    },
+  },
+  computed: {
+    relatedProducts() {
+      if (this.product === null) {
+        return [];
+      }
+      let related = [];
+      let count = 0;
+      this.products.forEach((product) => {
+        if (product.id != this.product.id && count < 5) {
+          related.push(product);
+          count++;
+        }
+      });
+      return related;
     },
   },
 };
