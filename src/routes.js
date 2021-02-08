107dcc85dec8589f0fd4ee7ced6b3b7b3a32908f
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-02-08 17:43:12 +0600

    159 Redirecting

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Cart from './Cart.vue';
 import ViewProduct from './ViewProduct.vue';
 
 export const routes = [
+    { path: '/products/:productId/view', redirect: { name: 'viewProduct' } },
     { path: '', component: ProductList },
     { path: '/cart', component: Cart },
     { path: '/products/:productId', props:true, name: 'viewProduct', component: ViewProduct },
