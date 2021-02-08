ecfcbb81833cb5f932b9b152d281d187e6608888
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-02-08 17:45:42 +0600

    404 page

--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,6 @@ export const routes = [
     { path: '', component: ProductList },
     { path: '/cart', component: Cart , alias: '/shopping-cart'},
     { path: '/products/:productId', props:true, name: 'viewProduct', component: ViewProduct },
+    // 404 page
     { path: '*', component: { template: '<h1>Page not found!</h1>' } }
 ]
